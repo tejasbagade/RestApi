92b95037d3f2e3fe1071c500bf43ca49cce47e5b
tejasbagade <tejasbagade@hotmail.com>
2021-02-21 01:47:11 +0000

    First commit to devlop

--- rest/src/main/java/com/example/rest/RestApplication.java
+++ rest/src/main/java/com/example/rest/RestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Demo");
 		SpringApplication.run(RestApplication.class, args);
 	}
 
--- rest/src/main/java/com/example/rest/repositories/EmployeeRepository.java
+++ rest/src/main/java/com/example/rest/repositories/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.example.rest.repositories;
 
 import com.example.rest.entities.Employee;
+import com.sun.xml.bind.v2.runtime.output.SAXOutput;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
